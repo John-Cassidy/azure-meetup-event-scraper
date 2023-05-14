cf3d208f87f0233d099d3f1147832e9f8fca7403
John Cassidy <jpcassidy@outlook.com>
2023-05-14 16:42:19 -0400

    Get List of upcoming Boston Azure Events listed on Meetup

---
+++ azure-meetup-event-scraper/GroupEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azure_meetup_event_scraper {
+    public class GroupEvent {
+        public int Id { get; set; }
+        public string Link { get; set; }
+        public string Title { get; set; }
+
+        public EventType Type { get; set; }
+    }
+
+    public enum EventType {
+        NBA = 1,
+        VBA = 2,
+        BA = 3
+    }
+}
--- azure-meetup-event-scraper/MeetupEventScraperFunction.cs
+++ azure-meetup-event-scraper/MeetupEventScraperFunction.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace azure_meetup_event_scraper
 {
@@ -17,19 +20,53 @@ namespace azure_meetup_event_scraper
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            //log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            //string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            //return new OkObjectResult(responseMessage);
+
+            Regex _regex = new Regex("[^0-9]");
+
+            List<GroupEvent> groupEvents = new List<GroupEvent>();
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc = web.Load("https://www.meetup.com/bostonazure/events");
+
+            // extract each event id, link, and title
+            foreach (HtmlNode eventsNodes in doc.DocumentNode.SelectNodes("//div[@class='card card--hasHoverShadow eventCard border--none border--none buttonPersonality']")) {
+
+                HtmlNode? eventNode = eventsNodes.SelectSingleNode("//a[@class='eventCard--link']");
+                if (eventNode != null) {
+                    GroupEvent groupEvent = new GroupEvent();
+
+                    groupEvent.Title = eventNode.InnerText;
+
+                    HtmlAttribute eventLink = eventNode.Attributes["href"];
+                    if (eventLink.Value.Contains("a")) {
+                        groupEvent.Link = eventLink.Value;
+                    }
+
+                    if (int.TryParse(_regex.Replace(eventLink.Value, ""), out var result)) {
+                        groupEvent.Id = result;
+                    }
+
+                    groupEvent.Type = groupEvent.Title.Contains("virtual", StringComparison.OrdinalIgnoreCase) ? EventType.VBA : EventType.NBA;
+
+                    groupEvents.Add(groupEvent);
+                }
+            }
+
+            return new OkObjectResult(groupEvents);
         }
     }
 }
